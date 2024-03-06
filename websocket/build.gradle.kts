1e21d41b7f459fde3d31d491a938f058a6bef7cf
박진현 <wlsgus9936@gmail.com>
2024-03-07 03:22:06 +0900

    create

---
+++ websocket/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "com.giggle"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
